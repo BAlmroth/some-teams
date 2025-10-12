0c74b23a3da19fc9c88dc680c9e07a82b84e3a12
Benita Almroth <benita.almroth@hotmail.com>
2025-10-12 20:49:54 +0200

    add: indo.php, played with styling. Optimized everything.

--- about.php
+++ about.php
@@ -2,12 +2,33 @@
 require 'header.php';
 ?>
 
-    <h1>About the Creator</h1>
-<p>
-    My name is Benita Almroth and I am an aspiaring web developer from Gothenburg, Sweden. I am currently studying at YRGO Higher Vocational Education, in the first semester of a two year program.
-    
-    This is my first ever coding project, and i don't have any precious coding or programming experience. I chose this education because I wanted to broaden my horizon, and have always been interested in everyting techy. Therefore the endless posibillity of programming seems like a perfect way for me to prosper, where one day maybe I'll make websites for my favorite Game Developers! 
-    
-    Aside from my studies I enjoy video games, art and horror/mystery (movies, books, truecrime etc.).
+ <div class="about">
+    <h1 class="about-title">About Me</h1>
 
-</p>   
+   
+        <div class ="about-left">
+            <h3>Hi, i'm Benita!</h3>
+            <img src="images/Info.jpeg" alt="Me and my dog Bailey">
+        </div>
+
+        <div class ="about-right">
+           
+            <h3>A crashcourse about me</h3>
+                <ul>
+                    <li>Age: 23</li>
+                    <li>Favorite Food: My mothers Khao Soi</li>
+                    <li>Hobbies: Video games, art, horror/mystery, dogs</li>
+                    <li>Favorite Soccer Team: Bk Häcken "Biased because "
+                </ul>         
+            
+
+        </div>
+    </div> 
+    
+    <p>
+                I am an aspiring web developer from Gothenburg, Sweden. I am currently studying at YRGO Higher Vocational Education, in the first semester of a two year program.
+                
+                This is my first ever coding project, and i don't have any precious coding or programming experience. I chose this education because I wanted to broaden my horizon, and have always been interested in everyting techy. Therefore the endless posibillity of programming seems like a perfect way for me to prosper, where one day maybe I'll make websites for my favorite Game Developers! 
+                
+                Aside from my studies I enjoy video games, art and horror/mystery (movies, books, truecrime etc.). I am a real sucker for wine, derpy dogs and Radiohead.
+            </p>   
--- index.php
+++ index.php
@@ -4,16 +4,18 @@
 ?>
 
     <main>
-        <h1>Your Complete Guide to (Some) UEFA Women's Football teams</h1>
-            <img src="https://assets-cms.thescore.com/uploads/image/file/787081/w640xh480_GettyImages-2216708428.jpg?ts=1748090105" alt="UEFA cup">
-        <div class="logos">
-        <?php
-            foreach ($teams as $name => $details) { ?> 
+      
+            <img class="title-photo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/UEFA_Women%27s_Champions_League_logo.svg/1200px-UEFA_Women%27s_Champions_League_logo.svg.png" alt="UEFA champions league">
+          <h1>Your Complete Guide to (Some) UEFA Women's Football teams</h1>
+          <div class="logos">
+        <?php foreach ($teams as $name => $details) { ?> 
             <div class="logo">
                 <img
                 src="<?php echo $details['logo']; ?>" 
                 alt="team logo">
+                <h3><?php echo $name; ?></h2>
             </div>   
+
         <?php } ?>
         </div>
     </main>  
