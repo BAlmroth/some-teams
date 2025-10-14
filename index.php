32b5c10eda29828c5869c8da71850e078d3ab58d
Benita Almroth <benita.almroth@hotmail.com>
2025-10-14 08:46:08 +0200

    mod: about layout, content. Add: id for switch between files. overall improvements

--- index.php
+++ index.php
@@ -6,17 +6,21 @@
     <main>
       
             <img class="title-photo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/UEFA_Women%27s_Champions_League_logo.svg/1200px-UEFA_Women%27s_Champions_League_logo.svg.png" alt="UEFA champions league">
-          <h1>Your Complete Guide To The Qualified UEFA Champions League Teams of 2025/2026</h1>
+          <h1 class="index-title">Your Complete Guide Of The UEFA Champions League Teams of 2025/2026 League Phase</h1>
           <div class="logos">
+            
         <?php foreach ($teams as $name => $details) { ?> 
-            <div class="logo">
+            <a href="info.php#<?= $name ?>">
+                <div class="logo">
                 <img
                 src="<?php echo $details['logo']; ?>" 
                 alt="team logo">
                 <h3><?php echo $name; ?></h2>
             </div>   
-
+            </a>
         <?php } ?>
+        
+
         </div>
     </main>  
 
